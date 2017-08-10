fa893b2be668b4556cc58401cafb445fa8d22b55
github username <logemerson@gmail.com>
2017-08-10 08:22:20 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade|
+      grade[1].sort!
+    end
+  end
+end
